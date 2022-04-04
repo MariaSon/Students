deab510379085828c4001c9a327a99763d8b9b56
MariaSon <MashaSog@yandex.ru>
2022-04-04 18:57:24 +0300

    Students

--- Student.py
+++ Student.py
@@ -92,6 +92,46 @@ class Reviewer(Mentor):
     ...
 
 
+def mean_hw_grades(students, course):
+    persons = []
+    course_grades = []
+    grades_for_mean = []
+    for person in students:
+        if isinstance(person, Student):
+            persons.append(person)
+    for student in persons:
+        if course in student.courses_in_progress:
+            for grades in student.grades.keys():
+                if course in grades:
+                    course_grades.append(student.grades[course])
+        else:
+            print(f'У студента {student.name} нет курса {course}!')
+    for grades_list in course_grades:
+        for grade in grades_list:
+            grades_for_mean.append(grade)
+    return f'Средняя оценка за домашнее задание по курсу {course} - {round(mean(grades_for_mean), 2)}'
+
+
+def mean_lecturer_grades(lectors, course):
+    lectors_list = []
+    lectors_grades = []
+    lectors_grades_for_mean = []
+    for person in lectors:
+        if isinstance(person, Lecturer):
+            lectors_list.append(person)
+    for lector in lectors_list:
+        if course in lector.courses_attached:
+            for grades in lector.marks.keys():
+                if course in grades:
+                    lectors_grades.append(lector.marks[course])
+        else:
+            print(f'Лектор {lector.name} не читает лекции по курсу {course}!')
+    for grades_list in lectors_grades:
+        for grade in grades_list:
+            lectors_grades_for_mean.append(grade)
+    return f'Средняя оценка за лекции по курсу {course} - {round(mean(lectors_grades_for_mean), 2)}'
+
+
 Olga_student = Student('Olga', 'Gromova', 'female')
 Olga_student.courses_in_progress += ['Python', 'HTML', 'C#']
 
@@ -108,13 +148,29 @@ Dmitriy_lector = Lecturer('Dmitriy', 'Ivanov')
 Dmitriy_lector.courses_attached += ['Python', 'HTML']
 
 Roman_lector = Lecturer('Roman', 'Liskov')
-Roman_lector.courses_attached += ['C#']
+Roman_lector.courses_attached += ['C#', 'HTML']
 
 Oleg_reviewer = Reviewer('Oleg', 'Fedorov')
 Oleg_reviewer.courses_attached += ['Python', 'HTML']
 
 Boris_reviewer = Reviewer('Boris', 'Koreev')
 Boris_reviewer.courses_attached += ['C#']
+
+
+Olga_student.marks_lector(Dmitriy_lector, 'Python', 10)
+Olga_student.marks_lector(Dmitriy_lector, 'Python', 7)
+Olga_student.marks_lector(Dmitriy_lector, 'HTML', 9)
+Olga_student.marks_lector(Roman_lector, 'C#', 9)
+
+Lev_student.marks_lector(Dmitriy_lector, 'Python', 9)
+Lev_student.marks_lector(Dmitriy_lector, 'HTML', 8)
+
+Oleg_reviewer.rate_hw(Olga_student, 'Python', 9)
+Oleg_reviewer.rate_hw(Olga_student, 'HTML', 8)
+Oleg_reviewer.rate_hw(Lev_student, 'Python', 10)
+Oleg_reviewer.rate_hw(Lev_student, 'HTML', 7)
+
+Boris_reviewer.rate_hw(Olga_student, 'C#', 7)
 
 
 print('-------------')
@@ -133,3 +189,15 @@ print('-------------')
 print(Oleg_reviewer)
 print('-------------')
 print(Boris_reviewer)
+print('-------------')
+print(Dmitriy_lector.__lt__(Roman_lector))
+print('-------------')
+print(Olga_student.__lt__(Lev_student))
+print('-------------')
+print(mean_hw_grades([Olga_student, Lev_student], 'Python'))
+print('-------------')
+print(mean_hw_grades([Olga_student, Lev_student], 'C#'))
+print('-------------')
+print(mean_lecturer_grades([Dmitriy_lector, Roman_lector], 'HTML'))
+print('-------------')
+print(mean_lecturer_grades([Dmitriy_lector, Roman_lector], 'Python'))
